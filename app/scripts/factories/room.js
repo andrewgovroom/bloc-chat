f026de136b688c68802ede96a2ba5bb832d443c0
Andrew Lau <andrewgovroom@hotmail.com>
2017-03-08 14:28:16 -0800

    Listed Chat Rooms

--- app/scripts/factories/room.js
+++ app/scripts/factories/room.js
@@ -4,13 +4,11 @@
     var urlRef = 'https://bloc-chat-814ee.firebaseio.com';  
     var ref = firebase.database().ref().child("rooms");
     var rooms = $firebaseArray(ref);
-      
 
     return {
       all: rooms
     };
   }
-
 
   angular
     .module('blocChat')
